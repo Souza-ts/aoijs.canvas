ba0c5d634b02a5f706355b66f16e8c09853f7453
Souza <studioswhirl@gmail.com>
2025-06-13 14:12:09 -0300

    Algumas atualizações foram feitas!

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 <p align="center">
   <a href="https://discord.com/invite/HMUfMXDQsV">
-    <img width="100" src="https://github.com/aoijs/website/blob/master/assets/images/aoicanvas.png?raw=true" alt="aoi.canvas">
+    <img width="100" src="https://github.com/aoijs/website/blob/master/assets/images/aoicanvas.png?raw=true" alt="aoijs.canvas">
   </a>
 </p>
 
@@ -9,14 +9,14 @@
 <div align="center">
 
 [![Discord Server](https://img.shields.io/discord/773352845738115102?color=5865F2&logo=discord&logoColor=white)](https://discord.com/invite/HMUfMXDQsV)
-[![NPM Downloads](https://img.shields.io/npm/dt/@aoijs/aoi.canvas.svg?maxAge=3600)](https://www.npmjs.com/package/@aoijs/aoi.canvas)
-[![NPM Version](https://img.shields.io/npm/v/@aoijs/aoi.canvas.svg?maxAge=3600)](https://www.npmjs.com/package/@aoijs/aoi.canvas)
+[![NPM Downloads](https://img.shields.io/npm/dt/@aoijs/aoi.canvas.svg?maxAge=3600)](https://www.npmjs.com/package/aoijs.canvas)
+[![NPM Version](https://img.shields.io/npm/v/@aoijs/aoi.canvas.svg?maxAge=3600)](https://www.npmjs.com/package/aoijs.canvas)
 
 </div>
 
 ## About
 
-aoijs.canvas is a aoi.js extension that allows you to utilize canvas functions with aoi.js bot.
+aoi.canvas is an aoi.js extension that allows you to create and edit images with your aoi.js bot.
 
 ## Installation
 
@@ -24,23 +24,27 @@ aoijs.canvas is a aoi.js extension that allows you to utilize canvas functions w
 npm install aoijs.canvas
 ```
 
+## Documentation
+
+For detailed documentation, usage examples, and more, please visit the [aoi.canvas documentation](https://aoi.js.org/extensions/aoijs/aoicanvas).
+
 ## Setup
 
 ```js
-const { AoiClient } = require("aoi.js");
-const { AoiCanvas } = require("aoijs.canvas");
+const { AoiClient } = require('aoi.js');
+const { AoiCanvas } = require('aoijs.canvas');
 
 const client = new AoiClient({
-    intents: ["Guilds", "GuildMessages", "MessageContent"],
-    events: ["onMessage", "onInteractionCreate"],
-    prefix: "Discord Bot Prefix",
-    token: "Discord Bot Token",
+    intents: ['Guilds', 'GuildMessages', 'MessageContent'],
+    events: ['onMessage', 'onInteractionCreate'],
+    prefix: 'Discord Bot Prefix',
+    token: 'Discord Bot Token',
     database: {
-        type: "aoi.db",
-        db: require("@aoijs/aoi.db"),
-        dbType: "KeyValue",
-        tables: ["main"],
-        securityKey: "a-32-characters-long-string-here"
+        type: 'aoi.db',
+        db: require('@akarui/aoi.db'),
+        dbType: 'KeyValue',
+        tables: ['main'],
+        securityKey: 'a-32-characters-long-string-here'
     }
 });
 
@@ -48,8 +52,29 @@ const canvas = new AoiCanvas(client);
 
 // Ping Command Example
 client.command({
-    name: "ping",
+    name: 'ping',
     code: `Pong! $pingms`
+});
+
+// House Command Example
+client.command({
+    name: 'house',
+    code: `
+        $attachCanvas[mycanvas;house.png]
+        
+        $stroke[mycanvas;#03a9f4;10]
+
+        $closePath[mycanvas]
+        $lineTo[mycanvas;250;140]
+        $lineTo[mycanvas;150;60]
+        $moveTo[mycanvas;50;140]
+        $beginPath[mycanvas]
+
+        $fillRect[mycanvas;#03a9f4;130;190;40;60]
+        $strokeRect[mycanvas;#03a9f4;75;140;150;110;10]
+
+        $createCanvas[mycanvas;300;320]
+    `
 });
 ```
 
---
+++ dist/index.js
@@ -0,0 +1,107 @@
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __exportStar = (this && this.__exportStar) || function(m, exports) {
+    for (var p in m) if (p !== "default" && !Object.prototype.hasOwnProperty.call(exports, p)) __createBinding(exports, m, p);
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.AoiCanvas = exports.registerFonts = exports.log = void 0;
+const node_fs_1 = require("node:fs");
+const node_path_1 = require("node:path");
+const package_json_1 = __importDefault(require("../package.json"));
+const function_1 = require("./classes/function");
+const canvas_1 = require("@napi-rs/canvas");
+const gifencoder = require('gif-encoder-2');
+let funcs = 0;
+const log = (content, type = 'log') => {
+    const color = (type === 'error'
+        ? '\x1b[91m'
+        : (type === 'warn' ? '\x1b[33m' : ''));
+    return console[type](`\x1b[0m[\x1b[36maoi.canvas\x1b[0m]: ${color + content}\x1b[0m`);
+};
+exports.log = log;
+const loadFuncs = (client, path) => {
+    if (!(0, node_fs_1.existsSync)(path))
+        return 'notfound';
+    (0, node_fs_1.readdirSync)(path)?.forEach(file => {
+        const fpath = (0, node_path_1.resolve)(path, file);
+        if ((0, node_fs_1.statSync)(fpath).isDirectory())
+            loadFuncs(client, fpath);
+        if ((0, node_fs_1.statSync)(fpath).isFile() && (file.endsWith('.ts') || file.endsWith('.js')) && !file.endsWith('.d.ts')) {
+            try {
+                let func = require(fpath);
+                func = func.default ?? func;
+                if (func instanceof function_1.AoiFunction) {
+                    func.register(client);
+                    funcs++;
+                }
+                ;
+            }
+            catch (err) {
+                (0, exports.log)(`Error loading function at path '${fpath}'`, 'error');
+            }
+            ;
+        }
+        ;
+    });
+    return 'loaded';
+};
+const registerFonts = (fonts) => fonts.forEach(font => {
+    if (!(0, node_fs_1.existsSync)(font.src))
+        throw Error(`Invalid font source. (${font.src})`);
+    if ((0, node_fs_1.statSync)(font.src).isFile()) {
+        let filename = (0, node_path_1.basename)(font.src);
+        if (!['ttf', 'otf', 'woff', 'woff2'].find(x => filename.endsWith(`.${x}`)))
+            return;
+        filename = font.name ?? filename.split('.').slice(0, -1).join('.');
+        canvas_1.GlobalFonts.registerFromPath(font.src, filename);
+        if (canvas_1.GlobalFonts.has(filename))
+            (0, exports.log)(`Successfuly registered '${filename}' font.`);
+        else
+            (0, exports.log)(`Failed to register font '${filename}'`, 'error');
+    }
+    else
+        return (0, exports.registerFonts)((0, node_fs_1.readdirSync)(font.src).map(x => ({ src: (0, node_path_1.join)(font.src, x) })));
+});
+exports.registerFonts = registerFonts;
+class AoiCanvas {
+    constructor(client) {
+        loadFuncs(client, (0, node_path_1.join)(__dirname, './functions')) === 'loaded' ?
+            (0, exports.log)(`Successfuly loaded.`) :
+            (0, exports.log)('Failed to load the functions.', 'error');
+        try {
+            (async () => {
+                const res = await (await fetch('https://registry.npmjs.org/aoi.canvas', {
+                    headers: {
+                        'User-Agent': 'aoi.canvas',
+                    },
+                })).json();
+                if (!res.versions[package_json_1.default.version])
+                    return (0, exports.log)('This is a dev version. Some stuff may be incomplete or unstable.', 'warn');
+                if (package_json_1.default.version !== res['dist-tags'].latest)
+                    return (0, exports.log)('aoi.canvas is outdated!', 'warn');
+            })();
+        }
+        catch (e) {
+            (0, exports.log)('There was an error fetching aoi.canvas info on npm.', 'error');
+        }
+        ;
+    }
+}
+exports.AoiCanvas = AoiCanvas;
+;
+__exportStar(require("./typings"), exports);
+__exportStar(require("./classes"), exports);
+//# sourceMappingURL=index.js.map
